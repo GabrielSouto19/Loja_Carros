203058b7b355fa7bceb4d60ec89ef80e7a359a89
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2024-11-05 13:40:45 -0300

    começando a criar minhas buscas ao banco

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from cars.models import Car
 
 
 def cars_view(request):
-    return render(request,'cars.html')
+    cars_list = Car.objects.all()# Nesse caso não posso colocar o values se não começa a dar erro
+    # carro = Car.objects.filter(brand__name="Fiat")# pesquisa pelo nome do carro . como isso é uma Fk
+    # o __ indica que eu estou fazendo uma navegação entre tabelas
+    # context = {"cars_list":cars_list}
+    # carro = Car.objects.filter(model="Camaro")
+    # carro = Car.objects.filter(model__contains="Camaro")# é como se fosse o %LIKE% do sql
+    #carro = Car.objects.filter(model__icontains="camaro")#IGUAL O LIKE POREM ELE É CASE INSENCITIVE
+    context = {"cars_list":cars_list}
+    print(request)
+    return render(request,'cars.html',context)
     
