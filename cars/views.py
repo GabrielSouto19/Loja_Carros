f37d3d35f86ce99e96d0ec88b8fd47c12e7a6d2c
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2024-11-05 14:00:24 -0300

    terminando minha view

--- cars/views.py
+++ cars/views.py
@@ -5,14 +5,22 @@ from cars.models import Car
 
 
 def cars_view(request):
-    cars_list = Car.objects.all()# Nesse caso não posso colocar o values se não começa a dar erro
+    nome_carro = request.GET.get("search")
+    if nome_carro:
+        print(nome_carro)
+        carro = Car.objects.filter(brand__name = nome_carro)#filtrando por modelo
+        context = {"cars_list":carro}
+    else:
+        cars_list = Car.objects.all()# Nesse caso não posso colocar o values se não começa a dar erro
+        context = {"cars_list":cars_list}
+        
+    # carro = Car.objects.filter(brand__name = nome_carro)#filtrando por marca
     # carro = Car.objects.filter(brand__name="Fiat")# pesquisa pelo nome do carro . como isso é uma Fk
     # o __ indica que eu estou fazendo uma navegação entre tabelas
     # context = {"cars_list":cars_list}
     # carro = Car.objects.filter(model="Camaro")
     # carro = Car.objects.filter(model__contains="Camaro")# é como se fosse o %LIKE% do sql
     #carro = Car.objects.filter(model__icontains="camaro")#IGUAL O LIKE POREM ELE É CASE INSENCITIVE
-    context = {"cars_list":cars_list}
-    print(request)
+    # context = {"cars_list":carro}
     return render(request,'cars.html',context)
     
