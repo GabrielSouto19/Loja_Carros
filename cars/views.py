0a76fb2f21083a774fc6f3c5fecb2905571b4656
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2024-11-05 12:40:04 -0300

    create cars.html

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 def cars_view(request):
-    return HttpResponse("Ola mundo")
+    return render(request,'cars.html')
     
