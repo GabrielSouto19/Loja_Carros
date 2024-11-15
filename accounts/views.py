f67b9eb8b4bd8de74a7e255bc68d7a6c4dddcd87
GabrielSouto19 <gabrielsouto12344321@gmail.com>
2024-11-15 16:41:22 -0300

    Projeto parcialmente finalizado , falta ainda algumas validações e testes .Inseri autenticação de usuario

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
-
+from django.contrib.auth import authenticate,login,logout
 def register_view(request):
     if request.method == "GET":
         user_form = UserCreationForm()
@@ -8,11 +8,24 @@ def register_view(request):
         user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
-            return redirect("cars_list")
+            return redirect("login")
     return render(request,"register.html",{"user_form":user_form})
 
 
 def login_view(request):
-    login_form = AuthenticationForm()
+    if request.method == "GET":
+        login_form = AuthenticationForm()
+    else:
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request=request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect("cars_list")
+        else:
+            login_form = AuthenticationForm()
     return render(request,"login.html",{"login_form":login_form})
     
+def logout_view(request):
+    logout(request=request)
+    return redirect("cars_list")
--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars import views
-from accounts.views import register_view,login_view
+from accounts.views import register_view,login_view,logout_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/',view=views.cars_view,name="cars_list" ),
     path('new_car/',view=views.new_car_view,name="new_car" ),
     path('register/',view=register_view,name="register" ),
     path('login/',view=login_view,name="login" ),
+    path('logout/',view=logout_view,name="logout" ),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
